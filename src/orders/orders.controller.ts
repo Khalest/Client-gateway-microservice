bcfea22cbd783501accb925753e72e630f6e995b
LostoVayne <moises.barrera.dev@gmail.com>
2025-07-25 01:48:38 -0400

    feat: add orders module with controller, DTOs, and microservice integration

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { ORDER_SERVICE } from 'src/config';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createOrders(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAllOrders() {
+    return this.ordersClient.send('findAllOrders', {});
+  }
+
+  @Get(':id')
+  findOneOrder(@Param('id') id: string) {
+    return this.ordersClient.send('findOneOrder', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err as object);
+      }),
+    );
+  }
+}
